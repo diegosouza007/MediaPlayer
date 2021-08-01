43d4c7faeb21c1e09226a9e40167c5e106585bff
diegosouza007 <dsouza@yahoo.com>
2021-07-31 22:13:33 -0300

    adding more functions and some corrections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,73 @@
 // Getting DOM references from the index page
 
+const skipForward = document.getElementById('skip-forward');
+const fastForward = document.getElementById('fast-forward');
+const skipBack = document.getElementById('skip-back');
+const progress = document.getElementById('progress');
+const repeat = document.getElementById('repeat');
+const rewind = document.getElementById('rewind');
+const volume = document.getElementById('volume');
+const music = document.getElementById('music');
 const play = document.getElementById('play');
-const music = document.getElementById('music')
 
-// Adding event listenners for the buttons
+// Adding event listenners for the actions buttons
 
 play.addEventListener('click', playMusic);
+repeat.addEventListener('click', activeLoopMusic);
 
-// Player state (paused or playing)
+// Player status (paused or playing)
 
-let status = false;
+let isMusicPlaying = false;
 
-// Player functions
+// Repeat button status
+
+let isRepeatEnable = false;
+
+// ======= Player functions =======
+
+// Play/Pause the music
 
 function playMusic() {
 
     if (play.getAttribute('src') == './assets/img/buttons/play-circle.svg') {
         play.setAttribute('src', './assets/img/buttons/pause-circle.svg');
+        play.setAttribute('title', 'Pausar');
     } else {
         play.setAttribute('src', './assets/img/buttons/play-circle.svg');
+        play.setAttribute('title', 'Reproduzir');
+
     }
 
-    if (!status) {
+    if (!isMusicPlaying) {
         music.play();
     } else {
         music.pause();
     }
 
-    status = !status;
+    isMusicPlaying = !isMusicPlaying;
 
+}
+
+// Update the current time progress
+
+music.addEventListener("timeupdate", function() {
+
+    let duration = (music.duration / 60).toFixed(2).replace('.', ':');
+    let timer = music.currentTime;
+
+    progress.innerHTML = `00:00 / ${duration}`;
+});
+
+
+// Enable/Disable loop music
+
+function activeLoopMusic() {
+    if (!isRepeatEnable) {
+        music.loop = true;
+        repeat.setAttribute('src', './assets/img/buttons/repeat-active.svg');
+    } else {
+        music.loop = false;
+        repeat.setAttribute('src', './assets/img/buttons/repeat.svg');
+    }
+    isRepeatEnable = !isRepeatEnable;
 }
